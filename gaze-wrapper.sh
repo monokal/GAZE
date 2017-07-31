12411ec195383de4cb5071ae152f4fad43a1ab70
Daniel Middleton <monokal@users.noreply.github.com>
2017-07-31 13:35:58 +0100

    Update gaze-wrapper.sh

--- gaze-wrapper.sh
+++ gaze-wrapper.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+#            GAZE
+#  Turnkey Open Media Centre
+#              __        .-.
+#          .-"` .`'.    /\\|
+#  _(\-/)_" ,  .   ,\  /\\\/     =o O=
+# {(=o^O=)} .   ./,  |/\\\/
+# `-.(Y).-`  ,  |  , |\.-`
+#      /~/,_/~~~\,__.-`   =O o=
+#     ////~    // ~\\
+#   ==`==`   ==`   ==`
+#          monokal.io
+
 NAMESPACE=${GAZE_CLI_NAMESPACE:=monokal}
 IMAGE=${GAZE_CLI_IMAGE:=gaze}
 TAG=${GAZE_CLI_VERSION:=latest}
