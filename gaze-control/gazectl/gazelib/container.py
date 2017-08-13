1caa88f973b3cfac20154d078fcad3d7ff711fd7
Daniel Middleton <daniel.middleton@skybettingandgaming.com>
2017-08-13 23:25:07 +0100

    CP

---
+++ gaze-control/gazectl/gazelib/container.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+               GAZE
+     Turnkey Open Media Centre
+                 __        .-.
+             .-"` .`'.    /\\|
+     _(\-/)_" ,  .   ,\  /\\\/     =o O=
+    {(=o^O=)} .   ./,  |/\\\/
+    `-.(Y).-`  ,  |  , |\.-`
+         /~/,_/~~~\,__.-`   =O o=
+        ////~    // ~\\
+      ==`==`   ==`   ==`
+             monokal.io
+"""
+
+import sys
+
+import docker
+
+from .log import GazeLog
+
+
+class GazeContainer(object):
+    """ Provides methods to interact with Docker Containers. """
+
+    def __init__(self):
+        """
+        """
+
+        self.log = GazeLog()
+
+        # Instantiate a Docker client.
+        try:
+            self.docker_client = docker.DockerClient(
+                base_url='unix://var/run/docker.sock'
+            )
+
+        except docker.errors.APIError:
+            self.log("Failed to instantiate the Docker client.", 'exception')
+            sys.exit(1)
+
+    def run(self, name, image, environment, volumes, networks, ports,
+            restart_policy, labels, detach):
+        """
+        Run a Docker Container.
+
+        :param name: (str) The name of the container.
+        :param image: (str) The image to run.
+        :param environment: (dict or list) Environment variables.
+        :param volumes: (dict or list) Volumes to mount inside the container.
+        :param networks: (list) Names of networks to connect to.
+        :param ports: (dict) Ports to bind inside the container.
+        :param restart_policy: (dict) Restart policy of the container.
+        :param labels: (dict or list) Labels to set on the container.
+        :param detach: (bool) Run the container in the background.
+        :return logs: A Container object.
+        """
+
+        try:
+            container = self.docker_client.containers.run(
+                name=name,
+                image=image,
+                environment=environment,
+                volumes=volumes,
+                network=networks[0],
+                ports=ports,
+                restart_policy=restart_policy,
+                labels=labels,
+                detach=True
+            )
+
+        except docker.errors.ContainerError:
+            self.log(
+                "The {} container exited with a non-zero exit code.".format(
+                    name), 'exception')
+            sys.exit(1)
+
+        except docker.errors.ImageNotFound:
+            self.log(
+                "The {} image could not be found.".format(image), 'exception'
+            )
+            sys.exit(1)
+
+        except docker.errors.APIError as e:
+            self.log(
+                "The Docker daemon returned the following error:\n{}".format(e),
+                'exception'
+            )
+            sys.exit(1)
+
+        # If more than network was provided, attach the container to additional
+        # networks using "Network.connect()" once it's running. Skip the first
+        # element/network as we've already added it during run.
+        if len(networks) > 1:
+            for network_id in networks[1:]:
+                self.log(
+                    "Connecting \"{}\" to additional network \"{}\"...".format(
+                        name, network_id), 'info')
+
+                try:
+                    network = self.docker_client.networks.get(network_id)
+
+                except docker.errors.NotFound:
+                    self.log("The Docker Network ({}) was not found.".format(
+                        network_id), 'exception'
+                    )
+                    sys.exit(1)
+
+                except docker.errors.APIError as e:
+                    self.log(
+                        "The Docker daemon returned the following error:\n"
+                        "{}".format(e), 'exception'
+                    )
+                    sys.exit(1)
+
+                try:
+                    network.connect(container)
+
+                except docker.errors.APIError as e:
+                    self.log(
+                        "The Docker daemon returned the following error:\n"
+                        "{}".format(e), 'exception'
+                    )
+                    sys.exit(1)
+
+                self.log("    * Success!", 'success')
+
+        return container
