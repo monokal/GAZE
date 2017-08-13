da4041f4b07a0500828bd24e301bd1dee5574a7c
Daniel Middleton <daniel.middleton@skybettingandgaming.com>
2017-08-14 00:02:28 +0100

    CP

---
+++ gaze-control/gazectl/gazelib/network.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+               GAZE
+     Turnkey Open Media Centre
+                 __        .-.
+             .-"` .`'.    /\\|
+     _(\-/)_" ,  .   ,\  /\\\/     =o O=
+    {(=o^O=)} .   ./,  |/\\\/
+    `-.(Y).-`  ,  |  , |\.-`
+         /~/,_/~~~\,__.-`   =O o=
+        ////~    // ~\\
+      ==`==`   ==`   ==`
+             monokal.io
+"""
+
+import sys
+
+import docker
+
+from .log import GazeLog
+
+
+class GazeNetwork(object):
+    """ Provides methods to interact with Docker Networks. """
+
+    def __init__(self):
+        """
+        """
+
+        self.log = GazeLog()
+
+        # Instantiate a Docker client.
+        try:
+            self.docker_client = docker.DockerClient(
+                base_url='unix://var/run/docker.sock'
+            )
+
+        except docker.errors.APIError:
+            self.log("Failed to instantiate the Docker client.", 'exception')
+            sys.exit(1)
+
+    def get(self, network_id):
+        """
+        Get information about a Docker Network.
+
+        :param network_id: String: ID of the Docker Network.
+        :return volume: Docker Network object.
+        """
