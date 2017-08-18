0d70d27cfaab884478fdd453e299e6cd7ffe4f76
Daniel Middleton <daniel.middleton@skybettingandgaming.com>
2017-08-18 22:46:13 +0100

    Pushed by ./build.sh

--- build.sh
+++ build.sh
@@ -15,17 +15,17 @@
 set -e
 
 if ! hash docker 2>/dev/null; then
-    echo 'Docker is required to build GAZE. Please install it then try again.'
+    echo -e "\n[GAZE] Docker is required to build GAZE. Please install it then try again.\n"
     exit 1
 fi
 
-echo "\nBuilding & pushing the gazectl Docker Image...\n"
+echo -e "\n[GAZE] Building & pushing the gazectl Docker Image...\n"
 cd gaze-control && ./gazectl-build.sh; cd -
 
-echo "Building & pushing documentation..."
+echo -e "\n[GAZE] Building & pushing documentation...\n"
 mkdocs build --clean && mkdocs gh-deploy
 
-echo "\nPushing all changes to Git...\n"
+echo -e "\n[GAZE] Pushing all changes to Git...\n"
 git add -A && git commit -m "Pushed by ${0}" && git push
 
-echo "\nDone!\n"
+echo -e "\n[GAZE] Done!\n"
