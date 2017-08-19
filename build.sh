0968d450b8eab842476b29f97f364edc805ebcd5
Daniel Middleton <daniel.middleton@skybettingandgaming.com>
2017-08-19 01:55:28 +0100

    Pushed by ./build.sh

--- build.sh
+++ build.sh
@@ -18,24 +18,25 @@ NAMESPACE=${GAZECTL_NAMESPACE:='monokal'}
 IMAGE=${GAZECTL_IMAGE:='gazectl'}
 TAG=${GAZECTL_VERSION:='latest'}
 
-SC='\033[0;35m'
-EC='\033[0m'
+PURPLE='\033[0;35m'
+NONE='\033[0m'
 
 if ! hash docker 2>/dev/null; then
-    echo -e "${SC}[GAZE] Docker is required to build GAZE. Please install it then try again.${EC}"
+    echo -e "${PURPLE}[GAZE] Docker is required to build GAZE. Please install it then try again.${NONE}"
     exit 1
 fi
 
-echo -e "\n${SC}[GAZE] Building & pushing the gazectl Docker Image...${EC}\n"
-#cd gaze-control && ./gazectl-build.sh; cd -
-
+# Build & push the gazectl Docker Image.
+echo -e "\n${PURPLE}[GAZE] Building & pushing the gazectl Docker Image...${NONE}\n"
 docker build -t "${NAMESPACE}/${IMAGE}:${TAG}" gaze-control/ && \
 docker push "${NAMESPACE}/${IMAGE}:${TAG}"
 
-echo -e "\n${SC}[GAZE] Building & pushing documentation...${EC}\n"
+# Build & deploy documentation.
+echo -e "\n${PURPLE}[GAZE] Building & pushing documentation...${NONE}\n"
 mkdocs build --clean && mkdocs gh-deploy
 
-echo -e "\n${SC}[GAZE] Pushing all changes to Git...${EC}\n"
+# Push all changes to Git.
+echo -e "\n${PURPLE}[GAZE] Pushing all changes to Git...${NONE}\n"
 git add -A && git commit -m "Pushed by ${0}" && git push
 
-echo -e "\n${SC}[GAZE] Done!${EC}\n"
+echo -e "\n${PURPLE}[GAZE] Success!${NONE}\n"
