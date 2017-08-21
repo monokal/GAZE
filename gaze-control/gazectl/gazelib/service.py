05e1b4a09d10f831c6e49cff416e1197d4ea1953
Daniel Middleton <daniel.middleton@skybettingandgaming.com>
2017-08-21 15:23:15 +0100

    Pushed by ./build-master-all.sh

---
+++ gaze-control/gazectl/gazelib/service.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+               GAZE
+     Turnkey Open Media Center
+                 __        .-.
+             .-"` .`'.    /\\|
+     _(\-/)_" ,  .   ,\  /\\\/     =o O=
+    {(=o^O=)} .   ./,  |/\\\/
+    `-.(Y).-`  ,  |  , |\.-`
+         /~/,_/~~~\,__.-`   =O o=
+        ////~    // ~\\
+      ==`==`   ==`   ==`
+             monokal.io
+"""
+
+from .log import GazeLog
+
+
+class GazeService(object):
+    """ Provides methods to interact with GAZE services. """
+
+    def __init__(self):
+        self.log = GazeLog()
