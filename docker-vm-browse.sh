0d5d34b724bfd8de8ef86345d5d7ee11e4d4ee9d
Daniel Middleton <daniel.middleton@skybettingandgaming.com>
2017-08-22 15:46:51 +0100

    Pushed by ./build-master-all.sh

---
+++ docker-vm-browse.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+#            GAZE
+#  Turnkey Open Media Center
+#              __        .-.
+#          .-"` .`'.    /\\|
+#  _(\-/)_" ,  .   ,\  /\\\/     =o O=
+# {(=o^O=)} .   ./,  |/\\\/
+# `-.(Y).-`  ,  |  , |\.-`
+#      /~/,_/~~~\,__.-`   =O o=
+#     ////~    // ~\\
+#   ==`==`   ==`   ==`
+#          monokal.io
+
+set -e
+
+DEPS=( 'docker' )
+
+MAGENTA=$(tput setaf 5)
+GREEN=$(tput setaf 2)
+RED=$(tput setaf 1)
+NONE=$(tput sgr 0)
+
+for i in "${DEPS[@]}"; do
+    if ! hash "${i}" 2>/dev/null; then
+        echo -e "${RED}[GAZE] "${i}" is required to use this tool. Please install it then try again.${NONE}"
+        exit 1
+    fi
+done
+
+echo -e "${MAGENTA}[GAZE] The Docker VM's filesystem is mounted at \"/vm\".${NONE}"
+docker run --rm -it -v /:/vm alpine:edge sh
